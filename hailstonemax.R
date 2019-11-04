27258acdce0e558b95bb5932c4723eff6b980934
gkaur37 <54698652+gkaur37@users.noreply.github.com>
2019-11-03 23:25:56 -0800

    Add files via upload

---
+++ hailstonemax.R
@@ -0,0 +1,21 @@
+seqlen = c()
+for (i in 1:1000) {
+  n=i
+  counter=1
+  while (n != 1){
+    if (n%%2==0) {
+   		n<-as.integer(n/2)
+    }
+ 	  else {
+  		n<-as.integer(3*n+1)
+    }
+    counter = counter + 1
+  }
+  seqlen= c(seqlen, counter)
+}
+seqmax<-n
+maxval<-max(seqmax,n)
+plot(seqlen)
+png("seqlen.png")
+plot(seqmax)
+png("seqmax.png")
